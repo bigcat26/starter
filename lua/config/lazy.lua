8e65044c3d2ef7710811629abf31f9c604d54102
chris <rc.local@qq.com>
2025-02-02 23:43:21 +0800

    add copilot, cmake, nvim-dap etc

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,162 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "plugins" },
+    {
+      "github/copilot.vim",
+      event = "InsertEnter",
+      config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.keymap.set("i", "<C-j>", function()
+          return vim.fn["copilot#Accept"]()
+        end, { expr = true })
+      end,
+    },
+    {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      dependencies = {
+        { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+        { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+      },
+      build = "make tiktoken", -- Only on MacOS or Linux
+      opts = {
+        -- See Configuration section for options
+      },
+      -- See Commands section for default commands if you want to lazy load on them
+    },
+    {
+      "mfussenegger/nvim-dap",
+      keys = {
+        {
+          "<leader>dc",
+          function()
+            require("dap").continue()
+          end,
+          desc = "[D]ap [C]ontinue",
+        },
+        {
+          "<leader>db",
+          function()
+            require("dap").toggle_breakpoint()
+          end,
+          desc = "[D]ap [B]reakpoint",
+        },
+        {
+          "<leader>dt",
+          function()
+            require("dap").terminate()
+          end,
+          desc = "[D]ap [T]erminate",
+        },
+        {
+          "<leader>dr",
+          function()
+            require("dap").repl.toggle()
+          end,
+          desc = "[D]ap [R]epl",
+        },
+        {
+          "<leader>dn",
+          function()
+            require("dap").step_over()
+          end,
+          desc = "[D]ap [N]ext",
+        },
+        {
+          "<leader>di",
+          function()
+            require("dap").step_into()
+          end,
+          desc = "[D]ap [I]nto",
+        },
+        {
+          "<leader>do",
+          function()
+            require("dap").step_out()
+          end,
+          desc = "[D]ap [O]ut",
+        },
+        {
+          "<leader>dl",
+          function()
+            require("dap").run_last()
+          end,
+          desc = "[D]ap [L]ast",
+        },
+      },
+      config = function()
+        local dap = require("dap")
+        dap.adapters.gdb = {
+          type = "executable",
+          command = vim.fn.exepath("gdb"),
+          args = { "-i", "dap", "--eval-command", "set print pretty on" },
+        }
+        dap.configurations.cpp = {
+          {
+            name = "Launch",
+            type = "gdb",
+            request = "launch",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopAtBeginningOfMainSubprogram = false,
+          },
+          {
+            name = "Select and attach to process",
+            type = "gdb",
+            request = "attach",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            pid = function()
+              local name = vim.fn.input("Executable name (filter): ")
+              return require("dap.utils").pick_process({ filter = name })
+            end,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            name = "Attach to gdbserver :1234",
+            type = "gdb",
+            request = "attach",
+            target = "localhost:1234",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end,
+    },
+    "ravenxrz/DapInstall.nvim",
+    -- "ravenxrz/nvim-dap",
+    {
+      "rcarriga/nvim-dap-ui",
+      dependencies = {
+        "nvim-neotest/nvim-nio",
+        "mfussenegger/nvim-dap",
+      },
+      opts = function()
+        local dap, dapui = require("dap"), require("dapui")
+        dap.listeners.before.attach.dapui_config = function()
+          dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+          dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+          dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+          dapui.close()
+        end
+        return {
+          enabled = true,
+          enabled_commands = true,
+        }
+      end,
+    },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
